40f5f86e8142d72efca4158ff4f1c8b6eb030a9a
Sam23D <dev.sam23d@gmail.com>
2021-04-19 16:54:20 -0600

    Fixes text bug

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import { h, app } from "./js/hyperapp.js"
+import { h, app, text } from "./js/hyperapp.js"
 
 app({
   init: 0,
   view: state =>
     h('div', { class: ``}, [
-      "Hello world!"
+      text("Hello world!")
     ]),
   node: document.getElementById("app")
 })
